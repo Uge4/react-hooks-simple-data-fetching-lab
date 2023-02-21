3207a230f2a9305ee63764724abcd292563d88f1
eugene <eugeneholenstein@gmail.com>
2023-02-21 22:40:56 +1100

    completed simple react hooks fetch lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+import React, { useEffect, useState } from "react"
+
+function App(){
+    const [dog, setDogs] = useState(false)
+
+    useEffect(() => {
+
+        fetch(`https://dog.ceo/api/breeds/image/random`)
+        .then(r => r.json())
+        .then((data) => setDogs(data.message))
+        .catch(error => console.log(error));
+    }, [])
+
+    return (
+        <div> 
+            {dog ? (<img src={dog} alt="A Random Dog"></img>) : (<p>"Loading..."</p>)}
+        </div>
+       
+    )
+}
+
+export default App;
+
+
+
